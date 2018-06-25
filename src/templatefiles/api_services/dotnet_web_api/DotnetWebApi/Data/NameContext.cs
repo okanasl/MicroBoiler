6f2417b42b8e7ffd65e186f15fe4434a0a1cd376
DooMachine <okanaslankan@hotmail.com>
2018-06-26 02:15:16 +0300

    dev commit

---
+++ src/templatefiles/api_services/dotnet_web_api/DotnetWebApi/Data/NameContext.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetWebApi.Data
+{
+    public class NameContext : DbContext
+    {
+        /// <summary>
+        /// Constructor with options
+        /// </summary>
+        /// <param name="options"></param>
+        public NameContext(DbContextOptions<NameContext> options)
+            : base(options)
+        {
+        }
+        /// <summary>
+        /// Override Model Create
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ src/templatefiles/api_services/dotnet_web_api/DotnetWebApi/Startup.cs
@@ -0,0 +1,168 @@
+﻿
+
+
+#region if(authorization)
+    #region if(identityserver4)
+using IdentityServer4.AccessTokenValidation;
+    #endregion
+    #region if(keycloack)
+    // TODO: Implement keycloack
+    #endregion
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using DotnetWebApi.Services;
+#region if (eventbus)
+using MassTransit;
+using DotnetWebApi.Data;
+using Microsoft.EntityFrameworkCore;
+#endregion
+
+namespace DotnetWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+#region if (database)
+    string connectionString = "{{database:connectionString}}";
+    #region if (mssql)
+            services.AddDbContext<NameContext>(options =>
+                options.UseSqlServer(connectionString)
+                );
+    #endregion
+    #region if (mysql)
+            services.AddDbContext<NameContext>(options =>
+                options.UseMySql(connectionString)
+                );
+    #endregion
+    #region if (postgresql)
+            services.AddDbContext<NameContext>(options =>
+                options.UseNpgsql(connectionString)
+                );
+    #endregion
+#endregion
+#region if (cache)
+    #region if (redis)
+            services.AddDistributedRedisCache(option =>
+            {
+                option.Configuration = "{{redis_options:connection}}";
+                option.InstanceName = "{{redis_options:instance_name}}";
+            });
+    #endregion
+    #region if(memory)
+            services.AddMemoryCache(options => {
+                // Your options
+            });            
+    #endregion
+#endregion
+#region if (authorization)
+            // Use In your controller like
+            // [Authorize(Policy = "Your_Authorization")]
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Your_Authorization", policyUser =>
+                {
+                    // You may want change below
+                    // policyUser.RequireRole("fso.api.user");
+                });
+            });
+    #region if (identityserver4)
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+               .AddIdentityServerAuthentication(options =>
+               {
+                   options.Authority = "{{authorization:authority}}";
+                   options.ApiName = "{{authorization:api_name}}";
+                   options.ApiSecret = "{{authorization:api_secret}}";
+                   options.RequireHttpsMetadata = false;
+                   options.SupportedTokens = SupportedTokens.Both;
+               });
+    #endregion
+#endregion
+#region if (eventbus)
+    #region if (rabbitmq)
+            services.AddSingleton(provider => Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host("localhost", "/", h => {
+                    h.Username("{{rabbitmq:user:username}}");
+                    h.Password("{{rabbitmq:user:password}}");
+                });
+
+                cfg.ReceiveEndpoint(host, e =>
+                {
+                    e.PrefetchCount = 8;
+                    // Add Event Consumers Here
+                    // If you want Inject Services in Consumer add provider parameter like below.
+                    // e.Consumer<AnEventHappenedConsumer>(provider);
+                });
+            }));
+            services.AddSingleton<IPublishEndpoint>(provider => provider.GetRequiredService<IBusControl>());
+            services.AddSingleton<ISendEndpointProvider>(provider => provider.GetRequiredService<IBusControl>());
+            services.AddSingleton<IBus>(provider => provider.GetRequiredService<IBusControl>());
+            // Register with IHostedService To Start bus in Application Start and Stop when App Stops
+            services.AddSingleton<Microsoft.Extensions.Hosting.IHostedService, BusService>();        
+    #endregion
+#endregion
+            // You may want to change allowed origins for security.
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                    .AllowAnyOrigin()                   
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                    .Build());
+            });
+            services
+                .AddMvc()
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+#region if (authorization)
+            app.UseAuthentication();
+#endregion
+            app.UseCors("CorsPolicy");
+            // app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
